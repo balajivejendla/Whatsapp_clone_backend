49f35154a4a05e2a238bb0125ea7f70def5b5dcc
balajivejendla <127621870+balajivejendla@users.noreply.github.com>
2025-10-01 19:51:15 +0530

    backend

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:3000"],  # Add your frontend URL
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:3000","https://whatsapp-clone-two-alpha.vercel.app],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "Accept"],
